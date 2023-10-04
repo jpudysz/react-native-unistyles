c2b9bd9e177b20457647914d8a389a9e20e8cf27
Jacek Pudysz <jacekpudysz@gmail.com>
2023-10-05 00:36:13 +0200

    chore: remove node types from yarn.lock

--- src/hooks/useDimensions.web.ts
+++ src/hooks/useDimensions.web.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import type { ScreenSize } from '../types'
 
 export const useDimensions = (): ScreenSize => {
-    const timerRef = useRef<number>()
+    const timerRef = useRef<ReturnType<typeof setTimeout>>()
     const [screenSize, setScreenSize] = useState<ScreenSize>({
         width: window.innerWidth,
         height: window.innerHeight
