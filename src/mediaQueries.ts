64afeec9490efba43cef2b89f89405de07b8e7d2
Marcin Janus <janekmachine@gmail.com>
2023-10-10 08:46:41 +0200

    fix: media queires spaces

--- src/mediaQueries.ts
+++ src/mediaQueries.ts
@@ -3,31 +3,67 @@ export type MediaQueries =
 // For :w
 `:w[${bigint}]` |
 `:w[,${bigint}]` |
+`:w[, ${bigint}]` |
 `:w[${bigint},${bigint}]` |
+`:w[${bigint}, ${bigint}]` |
 
 // For :h
 `:h[${bigint}]` |
 `:h[,${bigint}]` |
+`:h[, ${bigint}]` |
 `:h[${bigint},${bigint}]` |
+`:h[${bigint}, ${bigint}]` |
 
 // Combinations of :w and :h
 `:w[${bigint}]:h[${bigint}]` |
 `:w[${bigint},${bigint}]:h[${bigint}]` |
+`:w[${bigint}, ${bigint}]:h[${bigint}]` |
 `:w[${bigint}]:h[${bigint},${bigint}]` |
+`:w[${bigint}]:h[${bigint}, ${bigint}]` |
 `:w[${bigint},${bigint}]:h[${bigint},${bigint}]` |
+`:w[${bigint}, ${bigint}]:h[${bigint},${bigint}]` |
+`:w[${bigint},${bigint}]:h[${bigint}, ${bigint}]` |
+`:w[${bigint}, ${bigint}]:h[${bigint}, ${bigint}]` |
 `:w[,${bigint}]:h[,${bigint}]` |
+`:w[, ${bigint}]:h[,${bigint}]` |
+`:w[,${bigint}]:h[, ${bigint}]` |
+`:w[, ${bigint}]:h[, ${bigint}]` |
 `:w[,${bigint}]:h[${bigint}]` |
+`:w[, ${bigint}]:h[${bigint}]` |
 `:w[${bigint}]:h[,${bigint}]` |
+`:w[${bigint}]:h[, ${bigint}]` |
 `:w[,${bigint}]:h[${bigint},${bigint}]` |
+`:w[, ${bigint}]:h[${bigint},${bigint}]` |
+`:w[,${bigint}]:h[${bigint}, ${bigint}]` |
+`:w[, ${bigint}]:h[${bigint}, ${bigint}]` |
 `:w[${bigint},${bigint}]:h[,${bigint}]` |
+`:w[${bigint}, ${bigint}]:h[,${bigint}]` |
+`:w[${bigint},${bigint}]:h[, ${bigint}]` |
+`:w[${bigint}, ${bigint}]:h[, ${bigint}]` |
 
 // Combinations of :h and :w
 `:h[${bigint}]:w[${bigint}]` |
 `:h[${bigint},${bigint}]:w[${bigint}]` |
+`:h[${bigint}, ${bigint}]:w[${bigint}]` |
 `:h[${bigint}]:w[${bigint},${bigint}]` |
+`:h[${bigint}]:w[${bigint}, ${bigint}]` |
 `:h[${bigint},${bigint}]:w[${bigint},${bigint}]` |
+`:h[${bigint}, ${bigint}]:w[${bigint},${bigint}]` |
+`:h[${bigint},${bigint}]:w[${bigint}, ${bigint}]` |
+`:h[${bigint}, ${bigint}]:w[${bigint}, ${bigint}]` |
 `:h[,${bigint}]:w[,${bigint}]` |
+`:h[, ${bigint}]:w[,${bigint}]` |
+`:h[,${bigint}]:w[, ${bigint}]` |
+`:h[, ${bigint}]:w[, ${bigint}]` |
 `:h[,${bigint}]:w[${bigint}]` |
+`:h[, ${bigint}]:w[${bigint}]` |
 `:h[${bigint}]:w[,${bigint}]` |
+`:h[${bigint}]:w[, ${bigint}]` |
 `:h[,${bigint}]:w[${bigint},${bigint}]` |
-`:h[${bigint},${bigint}]:w[,${bigint}]`
+`:h[, ${bigint}]:w[${bigint},${bigint}]` |
+`:h[,${bigint}]:w[${bigint}, ${bigint}]` |
+`:h[, ${bigint}]:w[${bigint}, ${bigint}]` |
+`:h[${bigint},${bigint}]:w[,${bigint}]` |
+`:h[${bigint}, ${bigint}]:w[,${bigint}]` |
+`:h[${bigint},${bigint}]:w[, ${bigint}]` |
+`:h[${bigint}, ${bigint}]:w[, ${bigint}]`
