037c95e5e8cbc30d257eb29e23ea92502a98b1b5
Jacek Pudysz <jacekpudysz@gmail.com>
2023-10-25 16:44:51 +0200

    feat: add support for server in useDimensions

--- src/hooks/useDimensions.web.ts
+++ src/hooks/useDimensions.web.ts
@@ -1,7 +1,15 @@
 import { useEffect, useRef, useState } from 'react'
 import type { ScreenSize } from '../types'
+import { isServer } from '../utils'
 
 export const useDimensions = (): ScreenSize => {
+    if (isServer()) {
+        return {
+            width: 0,
+            height: 0
+        }
+    }
+
     const timerRef = useRef<ReturnType<typeof setTimeout>>()
     const [screenSize, setScreenSize] = useState<ScreenSize>({
         width: window.innerWidth,
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export { normalizeStyles } from './normalizeStyles'
 export * from './normalizer'
 export { getBreakpointFromScreenWidth, sortAndValidateBreakpoints, getValueForBreakpoint } from './breakpoints'
 export { proxifyFunction, parseStyle } from './styles'
+export { isServer } from './common'
 export {
     extractValues,
     getKeyForCustomMediaQuery,
