043f15123b95ff38a636fd970ef48d561b17efcc
Jacek Pudysz <jacekpudysz@gmail.com>
2023-11-06 13:48:25 +0100

    chore: path ssr

--- src/hooks/useDimensions.web.ts
+++ src/hooks/useDimensions.web.ts
@@ -1,17 +1,12 @@
 import { useEffect, useRef, useState } from 'react'
 import type { ScreenSize } from '../types'
-import { isServer } from '../utils'
 
 export const useDimensions = (): ScreenSize => {
     const timerRef = useRef<ReturnType<typeof setTimeout>>()
     const [screenSize, setScreenSize] = useState<ScreenSize>({
-        width: isServer
-            ? undefined
-            : window.innerWidth,
-        height: isServer
-            ? undefined
-            : window.innerHeight
-    } as ScreenSize)
+        width: window.innerWidth,
+        height: window.innerHeight
+    })
 
     useEffect(() => {
         const handleResize = () => {
