da4459564e39ab005dd229a2eaa927533ffff919
Jacek Pudysz <jacekpudysz@gmail.com>
2023-11-06 14:21:26 +0100

    fix: hydration ssr

--- src/UnistylesTheme.tsx
+++ src/UnistylesTheme.tsx
@@ -1,6 +1,5 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext } from 'react'
 import type { PropsWithChildren } from 'react'
-import { isServer } from './utils'
 
 interface UnistylesThemeProps extends PropsWithChildren {
     theme: any
@@ -11,16 +10,8 @@ export const UnistylesContext = createContext({})
 export const UnistylesTheme: React.FunctionComponent<UnistylesThemeProps> = ({
     theme,
     children
-}) => {
-    const [isClient, setIsClient] = useState(!isServer)
-
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
-
-    return (
-        <UnistylesContext.Provider value={theme}>
-            {isClient ? children : <React.Fragment /> }
-        </UnistylesContext.Provider>
-    )
-}
+}) => (
+    <UnistylesContext.Provider value={theme}>
+        {children}
+    </UnistylesContext.Provider>
+)
