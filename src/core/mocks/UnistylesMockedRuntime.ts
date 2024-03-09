f9840f385c1325a460f9fc0e37891660320cadc6
Jacek Pudysz <jacekpudysz@gmail.com>
2024-03-09 21:36:03 +0100

    feat: make mocks dynamic

--- src/core/mocks/UnistylesMockedRuntime.ts
+++ src/core/mocks/UnistylesMockedRuntime.ts
@@ -1,29 +1,45 @@
 // @ts-nocheck
 import { ScreenOrientation } from '../../common'
-import type { UnistylesBridge, UnistylesPlugin, UnistylesRegistry } from '../../types'
+import type { UnistylesBridge, UnistylesPlugin } from '../../types'
 import type { UnistylesThemes } from '../../global'
+import type { UnistylesMockedRegistry } from './UnistylesMockedRegistry'
+import type { UnistyleRegistry } from '../UnistyleRegistry'
 
 export class UnistylesMockedRuntime {
-    constructor(private unistylesBridge: UnistylesBridge, private unistylesRegistry: UnistylesRegistry) {}
+    private unistylesRegistry: UnistylesMockedRegistry
+
+    constructor(private unistylesBridge: UnistylesBridge, private unistylesRegistry: UnistyleRegistry) {
+        this.unistylesRegistry = unistylesRegistry as unknown as UnistylesMockedRegistry
+    }
 
     public get colorScheme() {
-        return this.unistylesBridge.colorScheme
+        return 'dark'
     }
 
     public get hasAdaptiveThemes() {
-        return this.unistylesBridge.hasAdaptiveThemes
+        return true
     }
 
     public get themeName() {
-        return this.unistylesBridge.themeName
+        return this.unistylesRegistry.themeNames.length > 0
+            ? this.unistylesRegistry.themeNames.at(0)
+            : undefined
     }
 
     public get contentSizeCategory() {
-        return this.unistylesBridge.contentSizeCategory
+        return 'unspecified'
     }
 
     public get breakpoint() {
-        return this.unistylesBridge.breakpoint
+        if (this.unistylesRegistry.sortedBreakpointPairs.length === 0) {
+            return undefined
+        }
+
+        const firstBreakpoint = this.unistylesRegistry.sortedBreakpointPairs.at(0)
+
+        return firstBreakpoint
+            ? firstBreakpoint.at(0)
+            : undefined
     }
 
     public get breakpoints() {
@@ -31,26 +47,37 @@ export class UnistylesMockedRuntime {
     }
 
     public get enabledPlugins() {
-        return this.unistylesBridge.enabledPlugins
+        return this.unistylesRegistry.plugins
     }
 
     public get screen() {
         return {
-            width: this.unistylesBridge.screenWidth,
-            height: this.unistylesBridge.screenHeight
+            width: 360,
+            height: 800
         }
     }
 
     public get insets() {
-        return this.unistylesBridge.insets
+        return {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        }
     }
 
     public get statusBar() {
-        return this.unistylesBridge.statusBar
+        return {
+            height: 24,
+            width: 800
+        }
     }
 
     public get navigationBar() {
-        return this.unistylesBridge.navigationBar
+        return {
+            height: 0,
+            width: 0
+        }
     }
 
     public get orientation() {
