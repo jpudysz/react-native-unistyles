0aeecca52ef7632dae1d71a5c286c3d2768ed6a2
Jacek Pudysz <jacekpudysz@gmail.com>
2024-03-09 21:36:03 +0100

    feat: make mocks dynamic

--- src/core/mocks/UnistylesMockedRegistry.ts
+++ src/core/mocks/UnistylesMockedRegistry.ts
@@ -12,10 +12,34 @@ export class UnistylesMockedRegistry {
 
     constructor(private unistylesBridge: UnistylesBridge) {}
 
-    public addThemes = (themes: UnistylesThemes) => this
-    public addBreakpoints = (breakpoints: UnistylesBreakpoints) => this
-    public addConfig = (config: UnistylesConfig) => this
-    public getTheme = (forName: keyof UnistylesThemes) => ({} as UnistylesThemes[keyof UnistylesThemes])
+    public addThemes = (themes: UnistylesThemes) => {
+        this.themes = themes
+        this.themeNames = Object.keys(themes) as Array<keyof UnistylesThemes>
+
+        return this
+    }
+    public addBreakpoints = (breakpoints: UnistylesBreakpoints) => {
+        this.breakpoints = breakpoints
+        this.sortedBreakpointPairs = Object
+            .entries(breakpoints)
+            .sort((breakpoint1, breakpoint2) => {
+                const [, value1] = breakpoint1
+                const [, value2] = breakpoint2
+
+                return value1 - value2
+            }) as Array<[keyof UnistylesBreakpoints, UnistylesBreakpoints[keyof UnistylesBreakpoints]]>
+
+        return this
+    }
+    public addConfig = (config: UnistylesConfig) => {}
+    public getTheme = (forName: keyof UnistylesThemes) => {
+        if (this.themeNames.length === 0) {
+            return {} as UnistylesThemes[keyof UnistylesThemes]
+        }
+
+        return this.themes[forName]
+    }
+
     public addPlugin = (plugin: UnistylesPlugin, notify: boolean = true) => {}
     public removePlugin = (plugin: UnistylesPlugin) => {}
     public updateTheme = (name: keyof UnistylesThemes, theme: UnistylesThemes[keyof UnistylesThemes]) => {}
