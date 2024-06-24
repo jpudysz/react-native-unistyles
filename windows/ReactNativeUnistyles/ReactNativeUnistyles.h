9f6b7b14447255e842a4e2329af353ac0a21fabc
Jacek Pudysz <jacekpudysz@gmail.com>
2024-06-24 23:13:09 +0200

    feat: finish implementation for win

--- windows/ReactNativeUnistyles/ReactNativeUnistyles.h
+++ windows/ReactNativeUnistyles/ReactNativeUnistyles.h
@@ -8,21 +8,17 @@
 #include <winrt/Microsoft.ReactNative.h>
 #include <winrt/Windows.UI.Xaml.h>
 #include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.Graphics.Display.h>
 #include <winrt/Windows.UI.ViewManagement.h>
 #include "UnistylesRuntime.h"
 #include <TurboModuleProvider.h>
 
 using namespace winrt::Windows::UI::ViewManagement;
+using namespace winrt::Windows::Graphics::Display;
 using namespace winrt::Microsoft::ReactNative;
 using namespace winrt::Windows::UI::Xaml;
 using namespace winrt::Windows::UI::Core;
 using namespace facebook;
-
-struct UIInitialInfo {
-    Dimensions screen;
-    std::string colorScheme;
-    std::string contentSizeCategory;
-};
 
 namespace winrt::ReactNativeUnistyles
 {
@@ -31,38 +27,32 @@ REACT_MODULE(Unistyles, L"Unistyles")
 struct Unistyles {
 
     REACT_INIT(Initialize)
-    void Initialize(ReactContext const &reactContext) noexcept {
+        void Initialize(ReactContext const& reactContext) noexcept {
         m_reactContext = reactContext;
 
-        /*m_reactContext.UIDispatcher().Post([this]() mutable {
+        m_reactContext.UIDispatcher().Post([this]() mutable {
             this->windowSizeChange = Window::Current().CoreWindow().SizeChanged(TypedEventHandler<CoreWindow, WindowSizeChangedEventArgs>([=](CoreWindow const sender, WindowSizeChangedEventArgs const&) {
-                auto bounds = Window::Current().Bounds();
-
                 if (this->unistylesRuntime != nullptr) {
-                    Dimensions screenDimensions = Dimensions{(int)bounds.Width, (int)bounds.Height};
-
-                    ((UnistylesRuntime*)this->unistylesRuntime)->handleScreenSizeChange(screenDimensions, this->getInsets(), this->getStatusBarDimensions(), this->getNavigationBarDimensions());
+                    ((UnistylesRuntime*)this->unistylesRuntime)->handleScreenSizeChange(getScreenDimensions(), std::nullopt, std::nullopt, std::nullopt);
                 }
             }));
 
-            UISettings settings;
-
-            this->colorValuesChange = settings.ColorValuesChanged(TypedEventHandler<UISettings, IInspectable>([this](UISettings const& sender, IInspectable const&){
+            this->colorValuesChange = UISettings().ColorValuesChanged(TypedEventHandler<UISettings, IInspectable>([this](UISettings const& sender, IInspectable const&) {
                 if (this->unistylesRuntime != nullptr) {
                     ((UnistylesRuntime*)this->unistylesRuntime)->handleAppearanceChange(this->getColorScheme());
                 }
             }));
-        });*/
+        });
     }
-    
+
     REACT_SYNC_METHOD(install)
-    bool install() noexcept {
+        bool install() noexcept {
         if (m_reactContext == nullptr) {
             return false;
         }
 
         jsi::Runtime* jsiRuntime = TryGetOrCreateContextRuntime(m_reactContext);
-        
+
         if (jsiRuntime == nullptr) {
             return false;
         }
@@ -76,95 +66,35 @@ struct Unistyles {
     }
 
     void registerUnistylesHostObject(jsi::Runtime& runtime, std::shared_ptr<facebook::react::CallInvoker> callInvoker) {
-        //std::promise<UIInitialInfo> uiInfoPromise;
-        //auto uiInfoFuture = uiInfoPromise.get_future();
-
-        //m_reactContext.UIDispatcher().Post([this, promise = std::move(uiInfoPromise)]() mutable {
-        //    UIInitialInfo uiMetadata;
-        //    auto bounds = Window::Current().Bounds();
-
-        //    uiMetadata.screen = Dimensions{(int)bounds.Width, (int)bounds.Height};
-        //    uiMetadata.colorScheme = this->getColorScheme();
-        //    uiMetadata.contentSizeCategory = UnistylesUnspecifiedScheme;
-
-        //    promise.set_value(std::move(uiMetadata));
-        //});
-
-        //UIInitialInfo uiInfo = uiInfoFuture.get();
-
-        auto unistylesRuntime = std::make_shared<UnistylesRuntime>(
-            runtime,
-            callInvoker
-        );
-
-        //unistylesRuntime->onThemeChange([this](std::string theme) {
-        //    winrt::hstring themeName = winrt::to_hstring(theme);
-
-        //    auto payload = winrt::Microsoft::ReactNative::JSValueObject{
-        //        {"type", "theme"},
-        //        {"payload", winrt::Microsoft::ReactNative::JSValueObject{{"themeName", winrt::to_string(themeName)}}}
-        //    };
-
-        //    this->OnThemeChange(payload);
-        //});
-
-        //unistylesRuntime.get()->onLayoutChange([this](std::string breakpoint, std::string orientation, Dimensions& screen, Dimensions& statusBar, Insets& insets, Dimensions& navigationBar) {
-        //    auto payload = winrt::Microsoft::ReactNative::JSValueObject{
-        //        {"type", "layout"},
-        //        {"payload", winrt::Microsoft::ReactNative::JSValueObject{
-        //            {"breakpoint", breakpoint},
-        //            {"orientation", orientation},
-        //            {"screen", winrt::Microsoft::ReactNative::JSValueObject{
-        //                {"width", screen.width},
-        //                {"height", screen.height}
-        //            }},
-        //            {"statusBar", winrt::Microsoft::ReactNative::JSValueObject{
-        //                {"width", statusBar.width},
-        //                {"height", statusBar.height}
-        //            }},
-        //            {"navigationBar", winrt::Microsoft::ReactNative::JSValueObject{
-        //                {"width", navigationBar.width},
-        //                {"height", navigationBar.height}
-        //            }},
-        //            {"insets", winrt::Microsoft::ReactNative::JSValueObject{
-        //                {"top", insets.top},
-        //                {"bottom", insets.bottom},
-        //                {"left", insets.left},
-        //                {"right", insets.right}
-        //            }},
-        //        }}
-        //    };
-
-        //    this->OnLayoutChange(payload);
-        //});
-
-        //unistylesRuntime.get()->onPluginChange([this]() {
-        //    auto payload = winrt::Microsoft::ReactNative::JSValueObject{
-        //        {"type", "plugin"}
-        //    };
-
-        //    this->OnPluginChange(payload);
-        //});
-
-        //unistylesRuntime.get()->onContentSizeCategoryChange([this](std::string contentSizeCategory) {
-        //    // not available on windows
-        //});
+        auto unistylesRuntime = std::make_shared<UnistylesRuntime>(runtime, callInvoker);
 
         this->unistylesRuntime = unistylesRuntime.get();
+
+        makeShared((UnistylesRuntime*)unistylesRuntime.get());
 
         auto hostObject = jsi::Object::createFromHostObject(runtime, unistylesRuntime);
 
         runtime.global().setProperty(runtime, "__UNISTYLES__", std::move(hostObject));
     }
 
-    REACT_EVENT(OnThemeChange, L"__unistylesOnChange")
-    std::function<void(winrt::Microsoft::ReactNative::JSValueObject const&)> OnThemeChange;
+    void makeShared(UnistylesRuntime* unistylesRuntime) {
+        unistylesRuntime->setScreenDimensionsCallback([this]() {
+            return getScreenDimensions();
+        });
 
-    REACT_EVENT(OnLayoutChange, L"__unistylesOnChange")
-    std::function<void(winrt::Microsoft::ReactNative::JSValueObject const&)> OnLayoutChange;
+        unistylesRuntime->setColorSchemeCallback([this]() {
+            return getColorScheme();
+        });
 
-    REACT_EVENT(OnPluginChange, L"__unistylesOnChange")
-    std::function<void(winrt::Microsoft::ReactNative::JSValueObject const&)> OnPluginChange;
+        m_reactContext.UIDispatcher().Post([this, unistylesRuntime]() {
+            auto screen = getScreenDimensions();
+
+            unistylesRuntime->screen = Dimensions({ screen.width, screen.height });
+            unistylesRuntime->pixelRatio = screen.pixelRatio;
+            unistylesRuntime->fontScale = screen.fontScale;
+            unistylesRuntime->colorScheme = getColorScheme();
+        });
+    }
 
     ~Unistyles() {
         if (this->unistylesRuntime != nullptr) {
@@ -172,13 +102,11 @@ struct Unistyles {
         }
 
         if (this->windowSizeChange) {
-            Window::Current().CoreWindow().SizeChanged(this->windowSizeChange);
+            this->windowSizeChange = { NULL };
         }
 
         if (this->colorValuesChange) {
-            UISettings settings;
-
-            settings.ColorValuesChanged(this->colorValuesChange);
+            this->colorValuesChange = { NULL };
         }
     }
 
@@ -189,16 +117,17 @@ struct Unistyles {
         winrt::event_token colorValuesChange{ NULL };
 
         std::string getColorScheme() {
-            auto uiSettings = UISettings();
-            auto background = uiSettings.GetColorValue(UIColorType::Background);
+            UISettings uiSettings;
 
-            bool isDark = background == winrt::Windows::UI::Colors::Black();
+            auto backgroundColor = uiSettings.GetColorValue(UIColorType::Background);
+
+            bool isDark = backgroundColor == Colors::Black();
 
             if (isDark) {
                 return UnistylesDarkScheme;
             }
 
-            bool isLight = background == winrt::Windows::UI::Colors::White();
+            bool isLight = backgroundColor == Colors::White();
 
             if (isLight) {
                 return UnistylesLightScheme;
@@ -207,16 +136,13 @@ struct Unistyles {
             return UnistylesUnspecifiedScheme;
         }
 
-        Insets getInsets() {
-            return Insets{ 0, 0, 0, 0 };
-        }
+        Screen getScreenDimensions() {
+            auto bounds = Window::Current().Bounds();
+            auto displayInfo = DisplayInformation::GetForCurrentView();
+            auto pixelRatio = displayInfo.LogicalDpi() / 96.0f;
+            float fontScale = UISettings().TextScaleFactor();
 
-        Dimensions getStatusBarDimensions() {
-            return {0, 0};
-        }
-
-        Dimensions getNavigationBarDimensions() {
-            return { 0, 0 };
+            return Screen({ (int)bounds.Width, (int)bounds.Height, pixelRatio, fontScale });
         }
 };
 
