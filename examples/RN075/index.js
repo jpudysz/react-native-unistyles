062a00942156cda6438c40c1af9c261e5960017a
Jacek Pudysz <jacekpudysz@gmail.com>
2024-08-13 18:48:54 +0200

    feat: replace yarn with bun

--- examples/RN075/index.js
+++ examples/RN075/index.js
@@ -1,7 +1,6 @@
 
 import { AppRegistry } from 'react-native'
-// import { App } from './App'
-import { App } from './Runtime'
+import { App } from './App'
 import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
