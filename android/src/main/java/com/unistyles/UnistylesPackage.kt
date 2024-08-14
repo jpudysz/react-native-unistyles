73576085af1f73a56eca09a9eb6bb388a883665a
Jacek Pudysz <jacekpudysz@gmail.com>
2024-08-14 14:03:51 +0200

    feat: convert android to turbo package

--- android/src/main/java/com/unistyles/UnistylesPackage.kt
+++ android/src/main/java/com/unistyles/UnistylesPackage.kt
@@ -1,14 +1,12 @@
 package com.unistyles
 
 import android.util.Log
-import android.view.View
-import com.facebook.react.ReactPackage
+import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.ReactShadowNode
-import com.facebook.react.uimanager.ViewManager
+import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class UnistylesPackage: ReactPackage {
+class UnistylesPackage: TurboReactPackage() {
     companion object {
         private const val TAG: String = "Unistyles"
 
@@ -18,17 +16,13 @@ class UnistylesPackage: ReactPackage {
                 Log.i(TAG, "Installed Unistyles \uD83E\uDD84!")
             } catch (e: Throwable) {
                 Log.e(TAG, "Failed to load Unistyles C++ library! Is it properly linked?", e)
-
                 throw e
             }
         }
     }
 
-    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
-        return mutableListOf()
-    }
-
-    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
-        return mutableListOf()
+    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? = null
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+        return ReactModuleInfoProvider { emptyMap() }
     }
 }
