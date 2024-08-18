fac68b3bb7635b3f3e51a796880edd95f6796a10
Jacek Pudysz <jacekpudysz@gmail.com>
2024-08-18 12:28:31 +0200

    feat: enable immersive mode for ios

--- src/specs/index.ts
+++ src/specs/index.ts
@@ -4,14 +4,13 @@ import type { NavigationBar } from './NavigtionBar'
 import type { StatusBar, StatusBarHiddenAnimation } from './StatusBar'
 import type { UnistylesRuntime, MiniRuntime } from './UnistylesRuntime'
 import { StatusBarStyle, ColorScheme, Orientation } from './types'
+import { isIOS } from '../common'
 
 const HybridUnistylesRuntime = NitroModules.createHybridObject<UnistylesRuntime>('UnistylesRuntime')
 const HybridStatusBar = NitroModules.createHybridObject<StatusBar>('StatusBar')
 const HybridNavigationBar = NitroModules.createHybridObject<NavigationBar>('NavigationBar')
 
 HybridStatusBar.setStyle = (style: StatusBarStyle, animated?: boolean) => {
-    // it's best to forward the call to the native status bar
-    // as we need access to ViewController
     switch (style) {
         case StatusBarStyle.Light:
             return NativeStatusBar.setBarStyle('light-content', animated)
@@ -23,9 +22,11 @@ HybridStatusBar.setStyle = (style: StatusBarStyle, animated?: boolean) => {
 }
 
 HybridStatusBar.setHidden = (isHidden: boolean, animation?: StatusBarHiddenAnimation) => {
-    // it's best to forward the call to the native status bar
-    // as we need access to ViewController
     NativeStatusBar.setHidden(isHidden, animation)
+}
+
+if (isIOS) {
+    HybridUnistylesRuntime.setImmersiveMode = HybridStatusBar.setHidden
 }
 
 HybridUnistylesRuntime.statusBar = HybridStatusBar
