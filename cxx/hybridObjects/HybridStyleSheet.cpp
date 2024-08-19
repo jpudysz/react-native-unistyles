9f144886d08e60e0debef13b3e96c492272baea6
Jacek Pudysz <jacekpudysz@gmail.com>
2024-08-19 19:44:28 +0200

    feat: remove old cxx files

--- cxx/hybridObjects/HybridStyleSheet.cpp
+++ cxx/hybridObjects/HybridStyleSheet.cpp
@@ -1,5 +1,8 @@
 #include "HybridStyleSheet.h"
 
-jsi::Value HybridStyleSheet::create(jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) {
+jsi::Value HybridStyleSheet::create(jsi::Runtime &rt, const jsi::Value &thisVal, const jsi::Value *arguments, size_t count) {
+    assertThat(rt, count == 1, "StyleSheet.create must be called with one argument");
+    assertThat(rt, arguments[0].isObject(), "StyleSheet.create must be called with object or function");
+
     return jsi::Object(rt);
 }
