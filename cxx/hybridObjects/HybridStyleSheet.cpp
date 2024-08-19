8650cabe80a25070fef537a67d895df29830fb5e
Jacek Pudysz <jacekpudysz@gmail.com>
2024-08-19 20:13:02 +0200

    feat: start moving create logic

--- cxx/hybridObjects/HybridStyleSheet.cpp
+++ cxx/hybridObjects/HybridStyleSheet.cpp
@@ -1,8 +1,15 @@
 #include "HybridStyleSheet.h"
 
 jsi::Value HybridStyleSheet::create(jsi::Runtime &rt, const jsi::Value &thisVal, const jsi::Value *arguments, size_t count) {
-    assertThat(rt, count == 1, "StyleSheet.create must be called with one argument");
-    assertThat(rt, arguments[0].isObject(), "StyleSheet.create must be called with object or function");
+    helpers::assertThat(rt, count == 1, "StyleSheet.create must be called with one argument");
+    helpers::assertThat(rt, arguments[0].isObject(), "StyleSheet.create must be called with object or function");
+    
+    auto styleSheetId = thisVal.asObject(rt).getProperty(rt, "__id");
 
+    // this might happen only when hot reloading
+    if (!styleSheetId.isUndefined()) {
+        // styleSheetRegistry.remove(styleSheetId.asNumber());
+    }
+    
     return jsi::Object(rt);
 }
