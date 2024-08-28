9b876354af4a346396a8c7ded1a14ec344812b17
Hubert Bieszczad <brentlokk@gmail.com>
2024-08-28 09:49:34 +0200

    feat: basic dynamic styles

--- expo-example/App.tsx
+++ expo-example/App.tsx
@@ -1,10 +1,28 @@
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import './unistyles'
+import { StyleSheet } from 'react-native-unistyles'
+import { useState } from 'react'
 
 export default function App() {
+    const [state, setState] = useState(0)
+
     return (
-    <View>
-        <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+        <View>
+            <Text style={stylesheet.text}>Static red</Text>
+            <Text style={stylesheet.dynamicText(state)}>Dynamic red / blue</Text>
+            <Button
+                title={`Clicked ${state} times`}
+                onPress={() => setState(state + 1)}
+            />
+        </View>
     )
 }
+
+const stylesheet = StyleSheet.create({
+    text: {
+        color: 'red'
+    },
+    dynamicText: (test: number) => ({
+        color: test % 2 === 0 ? 'red' : 'blue'
+    })
+})
