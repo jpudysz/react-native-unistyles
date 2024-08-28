37b5d19c723773fc0b1d1c87d44abeb9a152cafc
Hubert Bieszczad <brentlokk@gmail.com>
2024-08-28 12:58:43 +0200

    feat: themes and breakpoints

--- web/index.ts
+++ web/index.ts
@@ -1,8 +1,9 @@
 import { create } from './create'
 import { UnistylesRuntime } from './runtime'
+import { UnistylesState } from './state'
 
 export const StyleSheet = {
-    configure: UnistylesRuntime.init,
+    configure: UnistylesState.init,
     create
 }
 
