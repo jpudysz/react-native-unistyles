f68c8be1c7d3e1aabd6a819a425ad8e85d7eb942
Jacek Pudysz <jacekpudysz@gmail.com>
2024-08-31 14:10:07 +0200

    feat: typescript support new variants

--- example/App.tsx
+++ example/App.tsx
@@ -4,6 +4,11 @@ import { StyleSheet, UnistylesRuntime } from 'react-native-unistyles'
 import './unistyles'
 
 export const App = () => {
+    styles.addVariants({
+        color: 'blue',
+        size: 'small'
+    })
+
     return (
         <View style={styles.container}>
             <Text>
@@ -19,5 +24,43 @@ const styles = StyleSheet.create(theme => ({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: theme.colors.backgroundColor,
+        variants: {
+            size: {
+                small: {
+                    width: 100,
+                    height: 100
+                },
+                medium: {
+                    width: 200,
+                    height: 200
+                },
+                large: {
+                    width: 300,
+                    height: 300
+                }
+            },
+            color: {
+                red: {
+                    backgroundColor: 'red'
+                },
+                green: {
+                    backgroundColor: 'green'
+                },
+                blue: {
+                    backgroundColor: '#a4a4e5'
+                }
+            }
+        },
+        compoundVariants: [
+            {
+                color: 'blue',
+                size: 'small',
+                styles: {
+                    width: '100%',
+                    height: '100%'
+                }
+            }
+        ],
+        uni__dependencies: [2]
     }
 }))
