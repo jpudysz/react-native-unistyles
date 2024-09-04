aee1eeaaaf0bce64aca55f17d331f822458cb0de
Hubert Bieszczad <brentlokk@gmail.com>
2024-09-04 13:01:51 +0200

    chore: return stylesheet values

--- web/utils.ts
+++ web/utils.ts
@@ -1,14 +1,24 @@
 import { ColorScheme, type AppThemeName} from '../src/specs/types'
+import type { UnistylesValues } from '../src/types'
 
 export const reduceObject = <TObj extends Record<string, any>, TReducer>(
     obj: TObj,
     reducer: (value: TObj[keyof TObj], key: keyof TObj) => TReducer,
 ) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, reducer(value as TObj[keyof TObj], key)])) as { [K in keyof TObj]: TReducer }
 
-export const toReactNativeClassName = (className: string) => ({
-    $$css: true,
-    'unistyles-class': className
-})
+export const toReactNativeClassName = (className: string, values: UnistylesValues) => {
+    const returnValue = {
+        $$css: true,
+        'unistyles-class': className
+    }
+
+    Object.defineProperties(returnValue, reduceObject(values, value => ({
+        value,
+        enumerable: false,
+    })))
+
+    return returnValue
+}
 
 export const keyInObject = <T extends Record<string, any>>(obj: T, key: PropertyKey): key is keyof T => key in obj
 
