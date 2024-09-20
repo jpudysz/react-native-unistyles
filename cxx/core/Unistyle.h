7cef5529fdea8ec8bf2bc784b481a143bbbd076f
Jacek Pudysz <jacekpudysz@gmail.com>
2024-09-20 09:29:13 +0200

    feat: add shadow registry, move stylesheet registry

---
+++ cxx/core/Unistyle.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "string"
+#include <jsi/jsi.h>
+#include <folly/dynamic.h>
+#include "Unistyles-Swift-Cxx-Umbrella.hpp"
+
+namespace margelo::nitro::unistyles::core {
+
+using namespace facebook;
+
+enum class UnistyleType {
+    Object,
+    DynamicFunction
+};
+
+struct DynamicFunctionMetadata {
+    size_t count;
+    std::vector<folly::dynamic> arguments;
+};
+
+struct Unistyle {
+    using Shared = std::shared_ptr<const Unistyle>;
+    
+    Unistyle(UnistyleType type, std::string styleKey, jsi::Object& rawObject)
+        : styleKey{styleKey}, type{type}, rawValue{std::move(rawObject)} {}
+
+    Unistyle(const Unistyle&) = delete;
+    Unistyle(Unistyle&& other) noexcept
+        : styleKey(std::move(other.styleKey)),
+          type(other.type),
+          rawValue(std::move(other.rawValue)),
+          parsedStyle(std::move(other.parsedStyle)),
+          dependencies(std::move(other.dependencies)),
+          dynamicFunctionMetadata(std::move(other.dynamicFunctionMetadata)) {}
+   
+    UnistyleType type;
+    std::string styleKey;
+    jsi::Object rawValue;
+    std::optional<jsi::Object> parsedStyle;
+    std::vector<UnistyleDependency> dependencies{};
+    std::optional<DynamicFunctionMetadata> dynamicFunctionMetadata = std::nullopt;
+};
+
+}
