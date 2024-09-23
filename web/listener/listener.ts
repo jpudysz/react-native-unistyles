dce369e140889f2294529bd018682de8ea9e2c1d
Hubert Bieszczad <brentlokk@gmail.com>
2024-09-23 08:25:57 +0200

    chore: implementation corrections

--- web/listener/listener.ts
+++ web/listener/listener.ts
@@ -2,13 +2,19 @@ import { UnistylesRuntime } from '../runtime'
 import { UnistyleDependency } from './dependencyList'
 
 export class UnistylesListenerBuilder {
+    private isInitialized = false
     private listeners = Array.from({ length: Object.keys(UnistyleDependency).length / 2 }, () => new Set<VoidFunction>())
 
-    emitChange = (dependency: UnistyleDependency) => {        
+    emitChange = (dependency: UnistyleDependency) => {
         this.listeners[dependency]?.forEach(listener => listener())
     }
 
     initListeners = () => {
+        if (this.isInitialized) {
+            return
+        }
+
+        this.isInitialized = true
         UnistylesRuntime.darkMedia?.addEventListener('change', () => this.emitChange(UnistyleDependency.ColorScheme))
         UnistylesRuntime.lightMedia?.addEventListener('change', () => this.emitChange(UnistyleDependency.ColorScheme))
         window.addEventListener('orientationchange', () => this.emitChange(UnistyleDependency.Orientation))
@@ -17,6 +23,10 @@ export class UnistylesListenerBuilder {
 
     addListeners = (dependencies: Array<UnistyleDependency>, listener: VoidFunction) => {
         dependencies.forEach(dependency => this.listeners[dependency]?.add(listener))
+
+        return () => {
+            dependencies.forEach(dependency => this.listeners[dependency]?.delete(listener))
+        }
     }
 }
 
