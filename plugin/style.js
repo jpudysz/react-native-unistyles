b673b5760a74c9a022cf9f945bfbde6180e656f0
Jacek Pudysz <jacekpudysz@gmail.com>
2024-10-04 11:27:12 +0200

    chore: add babel global counter for web dynamic functions

--- plugin/style.js
+++ plugin/style.js
@@ -1,4 +1,4 @@
-function getStyleObjectPath(t, node) {
+function getStyleObjectPath(t, node, state) {
     // {styles.container}
     if (t.isMemberExpression(node)) {
         return [node.object.name, node.property.name]
@@ -6,22 +6,39 @@ function getStyleObjectPath(t, node) {
 
     // [styles.container]
     if (t.isArrayExpression(node)) {
-        return node.elements.flatMap(element => getStyleObjectPath(t, element))
+        return node.elements.flatMap(element => getStyleObjectPath(t, element, state))
     }
 
     // [...styles.container]
     if (t.isSpreadElement(node)) {
-        return getStyleObjectPath(t, node.argument)
+        return getStyleObjectPath(t, node.argument, state)
     }
 
     // {{ ...styles.container }}
     if (t.isObjectExpression(node)) {
-        return node.properties.flatMap(prop => getStyleObjectPath(t, prop.argument))
+        return node.properties.flatMap(prop => getStyleObjectPath(t, prop.argument, state))
     }
 
     // {styles.container(arg1, arg2)}
     if (t.isCallExpression(node)) {
-        return getStyleObjectPath(t, node.callee)
+        const path = getStyleObjectPath(t, node.callee, state)
+        const lastPart = path.slice(-1)
+
+        if (!lastPart) {
+            return path
+        }
+
+        // for web increment counter for dynamic functions
+        if (state.file.webDynamicFunctions[lastPart]) {
+            node.arguments.push(t.numericLiteral(++state.file.webDynamicFunctions[lastPart]))
+
+            return path
+        }
+
+        state.file.webDynamicFunctions[lastPart] = 1
+        node.arguments.push(t.numericLiteral(1))
+
+        return path
     }
 
     return []
