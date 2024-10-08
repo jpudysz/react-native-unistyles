8e6a36beff4140b765fdb3a7b8026c7eaed1a551
Hubert Bieszczad <brentlokk@gmail.com>
2024-10-08 15:01:03 +0200

    fix: web dynamic functions

--- src/web/create.ts
+++ src/web/create.ts
@@ -16,6 +16,8 @@ type ListenToDependenciesProps = {
     args?: Array<any>
 }
 
+type WebUnistyle = ReturnType<typeof UnistylesRegistry.createStyles>
+
 export const create = (stylesheet: StyleSheetWithSuperPowers<StyleSheet>) => {
     const computedStylesheet = typeof stylesheet === 'function'
         ? stylesheet(UnistylesRuntime.theme, UnistylesRuntime.miniRuntime)
@@ -49,49 +51,42 @@ export const create = (stylesheet: StyleSheetWithSuperPowers<StyleSheet>) => {
 
     const styles = reduceObject(computedStylesheet, (value, key) => {
         if (typeof value === 'function') {
-            const classNameMap = new Map<number, string>()
-            const unistylesMap = new Map<number, TypeStyle>()
-            const disposeMap = new Map<number, VoidFunction | undefined>()
+            const webUnistyleByRef = new Map<HTMLElement, WebUnistyle>()
+            const disposeByRef = new Map<HTMLElement, VoidFunction | undefined>()
 
             return (...args: Array<any>) => {
-                const [id] = args.slice(-1)
-                const result = value(...args.slice(0, -1))
+                const [ref] = args.slice(-1)
+                const result = value(...args)
                 const variants = Object.fromEntries(getVariants({ [key]: result } as ReactNativeStyleSheet<StyleSheet>, lastlySelectedVariants))
                 const resultWithVariants = {
                     ...result,
                     ...variants[key]
                 }
-                const dispose = disposeMap.get(id)
-                const unistyles = unistylesMap.get(id)
-                const className = classNameMap.get(id)
 
-                if (unistyles && className) {
-                    dispose?.()
-                    UnistylesRegistry.updateStyles(unistyles, resultWithVariants, className)
-                    disposeMap.set(id, listenToDependencies({
+                if (ref instanceof HTMLElement) {
+                    const storedWebUnistyle = webUnistyleByRef.get(ref)
+                    const webUnistyle = storedWebUnistyle ?? UnistylesRegistry.createStyles(resultWithVariants, key)
+
+                    webUnistyleByRef.set(ref, webUnistyle)
+                    disposeByRef.get(ref)?.()
+                    disposeByRef.set(ref, listenToDependencies({
                         key,
                         value,
-                        unistyles,
-                        className,
+                        unistyles: webUnistyle.unistyles,
+                        className: webUnistyle.className,
                         args
                     }))
+                    ref.classList.add(webUnistyle.className)
 
-                    return toReactNativeClassName(className, resultWithVariants)
+                    if (storedWebUnistyle) {
+                        UnistylesRegistry.updateStyles(webUnistyle.unistyles, resultWithVariants, webUnistyle.className)
+                    }
+
+
+                    return
                 }
 
-                const entry = UnistylesRegistry.createStyles(resultWithVariants, key)
-
-                classNameMap.set(id, entry.className)
-                unistylesMap.set(id, entry.unistyles)
-                disposeMap.set(id, listenToDependencies({
-                    key,
-                    value,
-                    unistyles: entry.unistyles,
-                    className: entry.className,
-                    args
-                }))
-
-                return toReactNativeClassName(entry.className, resultWithVariants)
+                return toReactNativeClassName(null, resultWithVariants)
             }
         }
 
