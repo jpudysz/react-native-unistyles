2f3b322bf6768772e885916b9fc21b690bc13c23
Hubert Bieszczad <brentlokk@gmail.com>
2024-10-08 15:01:03 +0200

    fix: web dynamic functions

--- src/web/utils.ts
+++ src/web/utils.ts
@@ -6,11 +6,11 @@ export const reduceObject = <TObj extends Record<string, any>, TReducer>(
     reducer: (value: TObj[keyof TObj], key: keyof TObj) => TReducer,
 ) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, reducer(value as TObj[keyof TObj], key)])) as { [K in keyof TObj]: TReducer }
 
-export const toReactNativeClassName = (className: string, values: UnistylesValues) => {
-    const returnValue = {
+export const toReactNativeClassName = (className: string | null, values: UnistylesValues) => {
+    const returnValue = className ? {
         $$css: true,
         [className]: className
-    }
+    } : {}
 
     Object.defineProperties(returnValue, reduceObject(values, value => ({
         value,
