4f840a33002b49df60c586375aacd37112eb4463
Hubert Bieszczad <brentlokk@gmail.com>
2024-10-09 12:27:26 +0200

    feat: major refactor of web unistyles

---
+++ src/web/listener/listenToDependencies.ts
@@ -0,0 +1,46 @@
+import type { TypeStyle } from 'typestyle'
+import type { StyleSheet, StyleSheetWithSuperPowers } from '../../types/stylesheet'
+import type { UnistyleDependency } from '../../specs/NativePlatform'
+import { UnistylesListener } from './listener'
+import { UnistylesRuntime } from '../runtime'
+import { keyInObject } from '../utils'
+import { UnistylesRegistry } from '../registry'
+
+type ListenToDependenciesProps = {
+    key: PropertyKey,
+    unistyles: TypeStyle,
+    className: string,
+    stylesheet: StyleSheetWithSuperPowers<StyleSheet>
+    args?: Array<any>
+}
+
+export const listenToDependencies = ({ key, className, unistyles, args = [], stylesheet }: ListenToDependenciesProps) => {
+    const value = keyInObject(stylesheet, key) ? stylesheet[key] : undefined
+
+    if (!value) {
+        return
+    }
+
+    const dependencies = ('uni__dependencies' in value ? value['uni__dependencies'] : []) as Array<UnistyleDependency>
+
+    if (dependencies.length === 0) {
+        return
+    }
+
+    return UnistylesListener.addListeners(dependencies, () => {
+        const newComputedStylesheet = typeof stylesheet === 'function'
+            ? stylesheet(UnistylesRuntime.theme, UnistylesRuntime.miniRuntime)
+            : stylesheet
+
+        if (!keyInObject(newComputedStylesheet, key)) {
+            return
+        }
+
+        const value = newComputedStylesheet[key]!
+        const result = typeof value === 'function'
+            ? value(...args)
+            : value
+
+        UnistylesRegistry.updateStyles(unistyles, result, className)
+    })
+}
