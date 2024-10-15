1a5fed0d4cce32ac216e726a6be572236f8db462
Hubert Bieszczad <brentlokk@gmail.com>
2024-10-15 11:36:41 +0200

    style: remove empty line

--- src/web/variants/useVariants.ts
+++ src/web/variants/useVariants.ts
@@ -56,7 +56,6 @@ export const createUseVariants = (styles: ReactNativeStyleSheet<StyleSheet>, set
                     value: selectedVariants
                 })
             })
-
         })
     }
 
