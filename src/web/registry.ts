85f735e8d1341d0d424037dcdafda52656234363
Hubert Bieszczad <brentlokk@gmail.com>
2024-10-16 13:53:22 +0200

    feat: cache computed stylesheets

--- src/web/registry.ts
+++ src/web/registry.ts
@@ -1,8 +1,40 @@
 import { createTypeStyle, TypeStyle } from 'typestyle'
 import type { UnistylesValues } from '../types'
 import { convertToTypeStyle } from './convert'
+import type { StyleSheet, StyleSheetWithSuperPowers } from '../types/stylesheet'
+import { UnistylesRuntime } from './runtime'
+import { keyInObject } from './utils'
+import { UnistylesListener } from './listener'
 
 class UnistylesRegistryBuilder {
+    private stylesheets = new Map<StyleSheetWithSuperPowers<StyleSheet>, StyleSheet>()
+
+    getComputedStylesheet = (stylesheet: StyleSheetWithSuperPowers<StyleSheet>) => {
+        if (typeof stylesheet !== 'function') {
+            return stylesheet
+        }
+
+        const computedStylesheet = this.stylesheets.get(stylesheet)
+
+        if (computedStylesheet) {
+            return computedStylesheet
+        }
+
+        const createdStylesheet = stylesheet(UnistylesRuntime.theme, UnistylesRuntime.miniRuntime)
+        // @ts-expect-error uni__dependencies is hidden
+        const dependencies = Object.values(createdStylesheet).flatMap(value => keyInObject(value, 'uni__dependencies') ? value.uni__dependencies : [])
+
+        UnistylesListener.addListeners(dependencies, () => {
+            const newComputedStylesheet = stylesheet(UnistylesRuntime.theme, UnistylesRuntime.miniRuntime)
+
+            this.stylesheets.set(stylesheet, newComputedStylesheet)
+        })
+
+        this.stylesheets.set(stylesheet, createdStylesheet)
+
+        return createdStylesheet
+    }
+
     createStyles = (stylesheet: UnistylesValues, key: string) => {
         const unistyles = createTypeStyle()
         const typestyleStylesheet = convertToTypeStyle(stylesheet)
