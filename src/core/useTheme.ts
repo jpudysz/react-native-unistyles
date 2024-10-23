cbb05187aad5d1e5e67762c60b149eedc51c55dd
Jacek Pudysz <jacekpudysz@gmail.com>
2024-10-23 21:16:43 +0200

    chore: exclude files from bundle

--- src/core/useTheme.ts
+++ src/core/useTheme.ts
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react'
 import { UnistylesRuntime } from '../web'
 import { UnistylesListener } from '../web/listener'
 import { UnistyleDependency } from '../specs/NativePlatform'
+import type { UnistylesTheme } from '../types'
 
-export const useTheme = () => {
+export const useTheme = (): UnistylesTheme => {
     const [theme, setTheme] = useState(UnistylesRuntime.getTheme())
 
     useEffect(() => {
