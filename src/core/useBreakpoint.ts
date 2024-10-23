4eab6ccb8d615ca155a1d30286d3bccae5fd14cd
Hubert Bieszczad <brentlokk@gmail.com>
2024-10-23 14:26:30 +0200

    feat: create useBreakpoint and useTheme for web

--- src/core/useBreakpoint.ts
+++ src/core/useBreakpoint.ts
@@ -1,5 +1,18 @@
-export const useBreakpoint = () => {
-    // todo implement me
+import { useEffect, useState } from 'react'
+import { UnistylesRuntime } from '../web'
+import { UnistylesListener } from '../web/listener'
+import { UnistyleDependency } from '../specs'
 
-    return 'sm'
+export const useBreakpoint = () => {
+    const [breakpoint, setBreakpoint] = useState(UnistylesRuntime.breakpoint)
+
+    useEffect(() => {
+        const removeChangeListener = UnistylesListener.addListeners([UnistyleDependency.Breakpoints], () => setBreakpoint(UnistylesRuntime.breakpoint))
+
+        return () => {
+            removeChangeListener()
+        }
+    }, [])
+
+    return breakpoint
 }
