e5e971523871c960aa5e15fae5faacd4e0469af2
Jacek Pudysz <jacekpudysz@gmail.com>
2024-11-05 17:07:30 +0100

    feat: grab runtime executor and uimanager android

--- android/src/main/cxx/NativeUnistylesModule.h
+++ android/src/main/cxx/NativeUnistylesModule.h
@@ -1,19 +1,38 @@
 #pragma once
 
 #include <ReactCommon/BindingsInstallerHolder.h>
+#include <react/fabric/JFabricUIManager.h>
+#include <react/jni/JRuntimeExecutor.h>
+#include <react/renderer/scheduler/Scheduler.h>
 #include <fbjni/fbjni.h>
+#include <react/fabric/Binding.h>
 
 namespace margelo::nitro::unistyles {
 
 using namespace facebook;
+using namespace facebook::react;
 
-struct UnistylesModule : public jni::JavaClass<UnistylesModule> {
+struct UnistylesModule : public jni::HybridClass<UnistylesModule> {
     static constexpr auto kJavaDescriptor = "Lcom/unistyles/UnistylesModule;";
 
-    UnistylesModule() = default;
+    explicit UnistylesModule(
+        jni::alias_ref<jhybridobject> jThis,
+        jni::alias_ref<react::JRuntimeExecutor::javaobject> runtimeExecutorHolder,
+        jni::alias_ref<JFabricUIManager::javaobject> fabricUIManager
+    );
 
     static void registerNatives();
-    static jni::local_ref<react::BindingsInstallerHolder::javaobject> getBindingsInstaller(jni::alias_ref<UnistylesModule> jobj);
+    static jni::local_ref<jhybriddata> initHybrid(
+        jni::alias_ref<jhybridobject> jThis,
+        jni::alias_ref<JRuntimeExecutor::javaobject> runtimeExecutorHolder,
+        jni::alias_ref<JFabricUIManager::javaobject>
+    );
+
+    static jni::local_ref<BindingsInstallerHolder::javaobject> getBindingsInstaller(jni::alias_ref<UnistylesModule::jhybriddata> jThis);
+
+private:
+    std::shared_ptr<UIManager> _uiManager;
+    RuntimeExecutor _runtimeExecutor;
 };
 
 }
