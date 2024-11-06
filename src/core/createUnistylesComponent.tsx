05647dab282340d3bed5257d36c1226b4bbfe32c
Hubert Bieszczad <brentlokk@gmail.com>
2024-11-06 08:31:26 +0100

    feat: createUnistylesComponent on the web

--- src/core/createUnistylesComponent.tsx
+++ src/core/createUnistylesComponent.tsx
@@ -1,52 +1,41 @@
-import React, { useEffect, useRef, useState, type ComponentType } from 'react'
+import React, { useEffect, useState, type ComponentType } from 'react'
 import type { UnistylesTheme } from '../types'
 import { UnistylesRuntime } from '../specs'
 import { UnistyleDependency } from '../specs/NativePlatform'
 import type { PartialBy } from '../types/common'
-import { deepMergeObjects, extractSecrets, extractUnistyleDependencies } from '../web/utils'
 import { UnistylesListener } from '../web/listener'
-import { UnistylesRegistry } from '../web/registry'
-import { getVariants } from '../web/variants'
+import { UnistylesShadowRegistry } from '../web'
+import { equal } from '../web/utils'
 
 const SUPPORTED_STYLE_PROPS = ['style', 'contentContainerStyle'] as const
 const ALL_DEPENDENCIES = Object.values(UnistyleDependency).filter((dependency): dependency is UnistyleDependency => typeof dependency === 'number')
 
-export const createUnistylesComponent = <TProps extends Record<string, any>, TMappings extends Partial<TProps>>(Component: ComponentType<TProps>, mappings?: (theme: UnistylesTheme) => TMappings) => {
-    const getStyles = (styles: Record<string, any>) => {
-        // Not a unistyle
-        if (!Object.keys(styles).some(key => key.startsWith('__uni__'))) {
-            return styles
-        }
+const useShadowRegistry = (style?: Record<string, any>) => {
+    const [classNames, setClassNames] = useState<Array<string>>([])
+    const [ref] = useState(document.createElement('div'))
 
-        const secrets = extractSecrets(styles)
+    if (style) {
+        UnistylesShadowRegistry
+            .add(ref, style)
+            .then(newClassNames => {
+                if (equal(classNames, newClassNames)) {
+                    return
+                }
 
-        return deepMergeObjects(...secrets.map(({ __uni__key, __uni__stylesheet, __uni__args, __uni__variants }) => {
-            const newComputedStylesheet = UnistylesRegistry.getComputedStylesheet(__uni__stylesheet)
-            const style = newComputedStylesheet[__uni__key]!
-            const result = typeof style === 'function'
-                ? style(...__uni__args ?? [])
-                : style
-            const { variantsResult } = Object.fromEntries(getVariants({ variantsResult: result }, __uni__variants))
-            const resultWithVariants = deepMergeObjects(result, variantsResult ?? {})
-
-            return resultWithVariants
-        }))
+                setClassNames(newClassNames)
+            })
     }
 
-    return (props: PartialBy<TProps, keyof TMappings | typeof SUPPORTED_STYLE_PROPS[number]>) => {
-        const propsRef = useRef(props)
-        const [style, setStyle] = useState(getStyles(props.style ?? {}))
-        const [contentContainerStyle, setContentContainerStyle] = useState(getStyles(props.contentContainerStyle ?? {}))
-        const [mappingsProps, setMappingsProps] = useState(mappings?.(UnistylesRuntime.getTheme()))
+    return classNames
+}
 
-        propsRef.current = props
+export const createUnistylesComponent = <TProps extends Record<string, any>, TMappings extends Partial<TProps>>(Component: ComponentType<TProps>, mappings?: (theme: UnistylesTheme) => TMappings) => {
+    return (props: PartialBy<TProps, keyof TMappings | typeof SUPPORTED_STYLE_PROPS[number]>) => {
+        const [mappingsProps, setMappingsProps] = useState(mappings?.(UnistylesRuntime.getTheme()))
+        const styleClassNames = useShadowRegistry(props.style)
+        const contentContainerStyleClassNames = useShadowRegistry(props.contentContainerStyle)
 
         useEffect(() => {
-            const styleDependencies = extractUnistyleDependencies(props.style)
-            const contentContainerStyleDependencies = extractUnistyleDependencies(props.contentContainerStyle)
-
-            const disposeStyle = UnistylesListener.addListeners(styleDependencies, () => setStyle(getStyles(propsRef.current.style ?? {})))
-            const disposeContentContainerStyle = UnistylesListener.addListeners(contentContainerStyleDependencies, () => setContentContainerStyle(getStyles(propsRef.current.contentContainerStyle ?? {})))
             const disposeMappings = UnistylesListener.addListeners(ALL_DEPENDENCIES, () => {
                 if (!mappings) {
                     disposeMappings()
@@ -57,18 +46,24 @@ export const createUnistylesComponent = <TProps extends Record<string, any>, TMa
                 return setMappingsProps(mappings(UnistylesRuntime.getTheme()))
             })
 
-            return () => {
-                disposeStyle()
-                disposeContentContainerStyle()
-                disposeMappings()
-            }
+            return () => disposeMappings()
         }, [mappingsProps, props.style])
 
         const combinedProps = {
             ...mappingsProps,
             ...props,
-            ...props.style ? { style } : {},
-            ...props.contentContainerStyle ? { contentContainerStyle } : {}
+            ...props.style ? {
+                style: {
+                    $$css: true,
+                    'unistyles': styleClassNames.join(' ')
+                },
+            } : {},
+            ...props.contentContainerStyle ? {
+                style: {
+                    $$css: true,
+                    'unistyles': contentContainerStyleClassNames.join(' ')
+                },
+            } : {},
         } as unknown as TProps
 
         return <Component {...combinedProps} />
