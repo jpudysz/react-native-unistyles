85e10bdf21ce3e842c21257a896a5d8b92e08ebe
Jacek Pudysz <jacekpudysz@gmail.com>
2024-11-06 12:21:52 +0100

    feat: android pass all arguments to hybrids

--- android/src/main/cxx/NativeUnistylesModule.cpp
+++ android/src/main/cxx/NativeUnistylesModule.cpp
@@ -1,4 +1,8 @@
 #include "NativeUnistylesModule.h"
+#import <NitroModules/HybridObjectRegistry.hpp>
+#import "HybridUnistylesRuntime.h"
+#import "HybridStyleSheet.h"
+#import "HybridShadowRegistry.h"
 
 using namespace margelo::nitro::unistyles;
 using namespace facebook::react;
@@ -6,15 +10,19 @@ using namespace facebook::react;
 UnistylesModule::UnistylesModule(
     jni::alias_ref<UnistylesModule::jhybridobject> jThis,
     jni::alias_ref<react::JRuntimeExecutor::javaobject> runtimeExecutorHolder,
-    jni::alias_ref<JFabricUIManager::javaobject> fabricUIManager
-): _runtimeExecutor(runtimeExecutorHolder->cthis()->get()), _uiManager(fabricUIManager->getBinding()->getScheduler()->getUIManager()) {}
+    jni::alias_ref<JFabricUIManager::javaobject> fabricUIManager,
+    jni::alias_ref<JHybridNativePlatformSpec::javaobject> nativePlatform
+):  _runtimeExecutor(runtimeExecutorHolder->cthis()->get()),
+    _uiManager(fabricUIManager->getBinding()->getScheduler()->getUIManager()),
+    _nativePlatform(reinterpret_cast<Unistyles::HybridNativePlatformSpecCxx*>(nativePlatform->cthis())) {}
 
 jni::local_ref<UnistylesModule::jhybriddata> UnistylesModule::initHybrid(
     jni::alias_ref<UnistylesModule::jhybridobject> jThis,
     jni::alias_ref<JRuntimeExecutor::javaobject> runtimeExecutorHolder,
-    jni::alias_ref<JFabricUIManager::javaobject> fabricUIManager
+    jni::alias_ref<JFabricUIManager::javaobject> fabricUIManager,
+    jni::alias_ref<JHybridNativePlatformSpec::javaobject> nativePlatform
 ) {
-    return makeCxxInstance(jThis, runtimeExecutorHolder, fabricUIManager);
+    return makeCxxInstance(jThis, runtimeExecutorHolder, fabricUIManager, nativePlatform);
 }
 
 void UnistylesModule::registerNatives() {
@@ -26,10 +34,38 @@ void UnistylesModule::registerNatives() {
 
 jni::local_ref<BindingsInstallerHolder::javaobject> UnistylesModule::getBindingsInstaller(jni::alias_ref<UnistylesModule::jhybriddata> jthis) {
     return BindingsInstallerHolder::newObjectCxxArgs([jthis](jsi::Runtime& rt) {
+        // function is called on: first init and every live reload
+        // check if this is live reload, if so let's replace UnistylesRuntime with new runtime
+        auto hasUnistylesRuntime = HybridObjectRegistry::hasHybridObject("UnistylesRuntime");
+
+        if (hasUnistylesRuntime) {
+            HybridObjectRegistry::unregisterHybridObjectConstructor("UnistylesRuntime");
+            HybridObjectRegistry::unregisterHybridObjectConstructor("UnistylesStyleSheet");
+            HybridObjectRegistry::unregisterHybridObjectConstructor("UnistylesShadowRegistry");
+        }
+
         auto* cppInstance = reinterpret_cast<UnistylesModule*>(jthis.get());
         auto& runtimeExecutor = cppInstance->_runtimeExecutor;
         auto& uiManager = cppInstance->_uiManager;
+        auto runOnJSThread = [&](std::function<void(jsi::Runtime&)>&& callback) {
+            runtimeExecutor([&](jsi::Runtime &rt) {
+                callback(rt);
+            });
+        };
 
-        // todo init hybrids
+        // init hybrids
+        auto& nativePlatform = cppInstance->_nativePlatform;
+        auto unistylesRuntime = std::make_shared<HybridUnistylesRuntime>(*nativePlatform, rt, runOnJSThread);
+        auto styleSheet = std::make_shared<HybridStyleSheet>(unistylesRuntime, uiManager);
+
+        HybridObjectRegistry::registerHybridObjectConstructor("UnistylesRuntime", [unistylesRuntime]() -> std::shared_ptr<HybridObject>{
+            return unistylesRuntime;
+        });
+        HybridObjectRegistry::registerHybridObjectConstructor("UnistylesStyleSheet", [styleSheet]() -> std::shared_ptr<HybridObject>{
+            return styleSheet;
+        });
+        HybridObjectRegistry::registerHybridObjectConstructor("UnistylesShadowRegistry", []() -> std::shared_ptr<HybridObject>{
+            return std::make_shared<HybridShadowRegistry>();
+        });
     });
 }
--- android/src/main/cxx/NativeUnistylesModule.h
+++ android/src/main/cxx/NativeUnistylesModule.h
@@ -6,6 +6,7 @@
 #include <react/renderer/scheduler/Scheduler.h>
 #include <fbjni/fbjni.h>
 #include <react/fabric/Binding.h>
+#include "NativePlatform.h"
 
 namespace margelo::nitro::unistyles {
 
@@ -18,14 +19,16 @@ struct UnistylesModule : public jni::HybridClass<UnistylesModule> {
     explicit UnistylesModule(
         jni::alias_ref<jhybridobject> jThis,
         jni::alias_ref<react::JRuntimeExecutor::javaobject> runtimeExecutorHolder,
-        jni::alias_ref<JFabricUIManager::javaobject> fabricUIManager
+        jni::alias_ref<JFabricUIManager::javaobject> fabricUIManager,
+        jni::alias_ref<JHybridNativePlatformSpec::javaobject> nativePlatform
     );
 
     static void registerNatives();
     static jni::local_ref<jhybriddata> initHybrid(
         jni::alias_ref<jhybridobject> jThis,
         jni::alias_ref<JRuntimeExecutor::javaobject> runtimeExecutorHolder,
-        jni::alias_ref<JFabricUIManager::javaobject>
+        jni::alias_ref<JFabricUIManager::javaobject>,
+        jni::alias_ref<JHybridNativePlatformSpec::javaobject> nativePlatform
     );
 
     static jni::local_ref<BindingsInstallerHolder::javaobject> getBindingsInstaller(jni::alias_ref<UnistylesModule::jhybriddata> jThis);
@@ -33,6 +36,7 @@ struct UnistylesModule : public jni::HybridClass<UnistylesModule> {
 private:
     std::shared_ptr<UIManager> _uiManager;
     RuntimeExecutor _runtimeExecutor;
+    Unistyles::HybridNativePlatformSpecCxx* _nativePlatform;
 };
 
 }
