e2509ab17f4eb3cc48ba2c7f8eeaee125b6823ac
Jacek Pudysz <jacekpudysz@gmail.com>
2024-11-18 18:03:11 +0100

    feat: set min react native version

--- ios/UnistylesModuleOnLoad.h
+++ ios/UnistylesModuleOnLoad.h
@@ -4,12 +4,6 @@
     #error "Unistyles 3.0 requires your project to have New Architecture enabled."
 #endif
 
-#if __has_include(<ReactCommon/RCTTurboModuleWithJSIBindings.h>)
-    #import <ReactCommon/RCTTurboModuleWithJSIBindings.h>
-#else
-    #error "Unistyles 3.0 requires your project to use React Native 0.75 or higher."
-#endif
-
 #import <React/RCTEventEmitter.h>
 #import "TurboUnistyles/TurboUnistyles.h"
 #import <React/RCTSurfacePresenter.h>
