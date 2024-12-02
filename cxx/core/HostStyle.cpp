2a0bdb520e490e2085189b0728d553deff8e2a6e
Jacek Pudysz <jacekpudysz@gmail.com>
2024-12-02 10:44:48 +0100

    fix: no default variant is picked if user didnt pass any variant at all

--- cxx/core/HostStyle.cpp
+++ cxx/core/HostStyle.cpp
@@ -48,10 +48,12 @@ jsi::Function HostStyle::createAddVariantsProxyFunction(jsi::Runtime& rt) {
         auto parser = parser::Parser(this->_unistylesRuntime);
         auto pairs = helpers::variantsToPairs(rt, arguments[0].asObject(rt));
 
-        if (pairs == this->_variants) {
+        if (this->hasVariantsSet && pairs == this->_variants) {
             return jsi::Value::undefined();
         }
 
+        // new variants or empty variants
+        this->hasVariantsSet = true;
         this->_variants = pairs;
 
         parser.rebuildUnistylesWithVariants(rt, this->_styleSheet, this->_variants);
