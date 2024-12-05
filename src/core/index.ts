b8e36c04d667b04d51fb053d293305a1e06c4d6f
Jacek Pudysz <jacekpudysz@gmail.com>
2024-12-05 10:59:00 +0100

    feat: remove hacks from pressable wrapper

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,4 @@
 export { createUnistylesComponent } from './createUnistylesComponent'
 export { getBoundArgs } from './getBoundArgs'
-export { getId } from './getId'
 export * from './createUnistylesElement'
 export * from './Variants'
