ab664896d7a37c97c70a3d71039dc86c042049cd
Hubert Bieszczad <brentlokk@gmail.com>
2024-12-13 11:50:32 +0100

    fix: components exports

--- plugin/import.js
+++ plugin/import.js
@@ -30,8 +30,8 @@ function addUnistylesImport(t, path, state) {
         const newImport = t.importDeclaration(
             [t.importSpecifier(t.identifier(localName), t.identifier(name))],
             t.stringLiteral(state.opts.isLocal
-                ? state.file.opts.filename.split('react-native-unistyles').at(0).concat(`react-native-unistyles/src/components/native/${name}`)
-                : `react-native-unistyles/src/components/native/${name}`
+                ? state.file.opts.filename.split('react-native-unistyles').at(0).concat(`react-native-unistyles/components/native/${name}`)
+                : `react-native-unistyles/components/native/${name}`
             )
         )
 
