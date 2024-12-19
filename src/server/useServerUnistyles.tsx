faf45a8e91f73a92f01d25a3deee578751fbcfd5
Hubert Bieszczad <brentlokk@gmail.com>
2024-12-19 12:09:02 +0100

    feat: inject rnw styles

--- src/server/useServerUnistyles.tsx
+++ src/server/useServerUnistyles.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react'
+import { StyleSheet } from 'react-native'
 import { useServerInsertedHTML } from 'next/navigation'
 import { UnistylesWeb } from '../web'
 
@@ -15,12 +16,15 @@ export const useServerUnistyles = () => {
         if (!isServerInserted.current) {
             isServerInserted.current = true
 
+            // @ts-ignore
+            const rnwStyle = StyleSheet?.getSheet().textContent ?? ''
             const css = UnistylesWeb.registry.css.getStyles()
             const state = UnistylesWeb.registry.css.getState()
             UnistylesWeb.registry.reset()
 
             return (
                 <>
+                    <style id='rnw-style'>{rnwStyle}</style>
                     <style id='unistyles-web'>{css}</style>
                     <script id='unistyles-script'>{`window.UNISTYLES_STATE = ${JSON.stringify(state)}`}</script>
                 </>
