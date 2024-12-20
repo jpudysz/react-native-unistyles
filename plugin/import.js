ee7d92d08cad81a5962fb0dcef97fc1dbc5cc541
Jacek Pudysz <jacekpudysz@gmail.com>
2024-12-20 11:57:46 +0100

    feat: redesign variants

--- plugin/import.js
+++ plugin/import.js
@@ -38,27 +38,6 @@ function addUnistylesImport(t, path, state) {
         path.node.body.unshift(newImport)
     })
 
-    // import variants
-    if (state.file.hasVariants) {
-        const unistylesImport = path.node.body
-            .find(node => t.isImportDeclaration(node) && node.source.value === 'react-native-unistyles')
-
-        if (unistylesImport) {
-            // prevent duplications
-            if (!unistylesImport.specifiers.some(specifier => specifier.local.name === 'Variants')) {
-                unistylesImport.specifiers.push(t.importSpecifier(t.identifier('Variants'), t.identifier('Variants')))
-            }
-        } else {
-            // Add a new import declaration for "Variants" from "react-native-unistyles"
-            const newImport = t.importDeclaration(
-                [t.importSpecifier(t.identifier('Variants'), t.identifier('Variants'))],
-                t.stringLiteral('react-native-unistyles')
-            )
-
-            path.node.body.unshift(newImport)
-        }
-    }
-
     // cleanup
     nodesToRemove.forEach(node => path.node.body.splice(path.node.body.indexOf(node), 1))
 }
