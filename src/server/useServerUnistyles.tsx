0bee095a16fd0bab9d69c9596b4c6a7a4dcca909
Hubert Bieszczad <brentlokk@gmail.com>
2025-01-10 14:24:34 +0100

    chore: remove hydration script

--- src/server/useServerUnistyles.tsx
+++ src/server/useServerUnistyles.tsx
@@ -5,6 +5,7 @@ import { UnistylesWeb } from '../web'
 
 declare global {
     interface Window {
+        // @ts-ignore
         __UNISTYLES_STATE__: ReturnType<typeof UnistylesWeb.registry.css.getState>
     }
 }
@@ -36,5 +37,6 @@ export const useServerUnistyles = () => {
 
     if (typeof window !== 'undefined') {
         UnistylesWeb.registry.css.hydrate(window.__UNISTYLES_STATE__)
+        document.querySelector('#unistyles-script')?.remove()
     }
 }
