abbf6c7e8900a6565955febe2d6395a41bb54a97
Hubert Bieszczad <brentlokk@gmail.com>
2025-01-29 10:03:03 +0100

    feat: copy native components properties

--- src/core/createUnistylesImageBackground.tsx
+++ src/core/createUnistylesImageBackground.tsx
@@ -3,43 +3,48 @@ import type { Image, ImageBackground, ImageBackgroundProps } from 'react-native'
 import { UnistylesShadowRegistry } from '../specs'
 import { passForwardedRef } from './passForwardRef'
 import { maybeWarnAboutMultipleUnistyles } from './warn'
+import { copyComponentProperties } from '../utils'
 
-export const createUnistylesImageBackground = (Component: typeof ImageBackground) => React.forwardRef<ImageBackground, ImageBackgroundProps>((props, forwardedRef) => {
-    const storedImageRef = useRef<Image | null>(null)
+export const createUnistylesImageBackground = (Component: typeof ImageBackground) => {
+    const UnistylesImageBackground = React.forwardRef<ImageBackground, ImageBackgroundProps>((props, forwardedRef) => {
+        const storedImageRef = useRef<Image | null>(null)
 
-    useLayoutEffect(() => {
-        return () => {
-            if (storedImageRef.current) {
-                // @ts-ignore
-                UnistylesShadowRegistry.remove(storedImageRef.current)
-            }
-        }
-    }, [])
-
-    // @ts-expect-error we don't know the type of the component
-    maybeWarnAboutMultipleUnistyles(props.style, 'ImageBackground')
-    // @ts-ignore we don't know the type of the component
-    maybeWarnAboutMultipleUnistyles(props.imageStyle, 'ImageBackground')
-
-    return (
-        <Component
-            {...props}
-            ref={ref => {
-                passForwardedRef(props, ref, forwardedRef)
-
-                return () => {
+        useLayoutEffect(() => {
+            return () => {
+                if (storedImageRef.current) {
                     // @ts-ignore
-                    UnistylesShadowRegistry.remove(ref)
+                    UnistylesShadowRegistry.remove(storedImageRef.current)
                 }
-            }}
-            imageRef={ref => {
-                if (ref) {
-                    storedImageRef.current = ref
-                }
+            }
+        }, [])
 
-                // @ts-expect-error web types are not compatible with RN styles
-                UnistylesShadowRegistry.add(ref, props.imageStyle)
-            }}
-        />
-    )
-})
+        // @ts-expect-error we don't know the type of the component
+        maybeWarnAboutMultipleUnistyles(props.style, 'ImageBackground')
+        // @ts-ignore we don't know the type of the component
+        maybeWarnAboutMultipleUnistyles(props.imageStyle, 'ImageBackground')
+
+        return (
+            <Component
+                {...props}
+                ref={ref => {
+                    passForwardedRef(props, ref, forwardedRef)
+
+                    return () => {
+                        // @ts-ignore
+                        UnistylesShadowRegistry.remove(ref)
+                    }
+                }}
+                imageRef={ref => {
+                    if (ref) {
+                        storedImageRef.current = ref
+                    }
+
+                    // @ts-expect-error web types are not compatible with RN styles
+                    UnistylesShadowRegistry.add(ref, props.imageStyle)
+                }}
+            />
+        )
+    })
+
+    return copyComponentProperties(Component, UnistylesImageBackground)
+}
