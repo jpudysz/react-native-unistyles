d274bae441dd35cc046e37a9adea513739cacab3
Hubert Bieszczad <brentlokk@gmail.com>
2025-04-01 09:15:39 +0200

    fix: image, imageBackground source width and height

--- src/components/native/Image.tsx
+++ src/components/native/Image.tsx
@@ -1,4 +1,36 @@
-import { Image as NativeImage } from 'react-native'
-import { createUnistylesElement } from '../../core'
+import React, { type ComponentProps, forwardRef } from 'react'
+import { type ImageStyle, Image as NativeImage, type StyleProp, type ViewStyle } from 'react-native'
+import { getClassName } from '../../core'
+import { maybeWarnAboutMultipleUnistyles } from '../../core/warn'
+import type { UnistylesValues } from '../../types'
+import { copyComponentProperties } from '../../utils'
+import { createUnistylesRef, keyInObject } from '../../web/utils'
 
-export const Image = createUnistylesElement(NativeImage)
+type Props = ComponentProps<typeof NativeImage> & {
+    style?: UnistylesValues
+    imageStyle?: UnistylesValues
+}
+
+const UnistylesImage = forwardRef<unknown, Props>((props, forwardedRef) => {
+    const classNames = getClassName(props.style)
+    const ref = createUnistylesRef(classNames, forwardedRef)
+    const hasWidthStyle = typeof props.style === 'object' && keyInObject(props.style, 'width')
+    const hasHeightStyle = typeof props.style === 'object' && keyInObject(props.style, 'height')
+
+    maybeWarnAboutMultipleUnistyles(props.style as ViewStyle, 'Image')
+
+    return (
+        <NativeImage
+            {...props}
+            style={[
+                classNames,
+                // Clear inline width and height extracted from source
+                hasWidthStyle && { width: '' },
+                hasHeightStyle && { height: '' }
+            ] as StyleProp<ImageStyle>}
+            ref={ref}
+        />
+    )
+})
+
+export const Image = copyComponentProperties(NativeImage, UnistylesImage)
--- src/web/utils/index.ts
+++ src/web/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './common'
 export * from './unistyle'
+export * from './createUnistylesRef'
