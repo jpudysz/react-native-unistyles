2534e9e95e7e89c2e85125c29ed67252c8134a25
hoonjoo-park <hoonjoo94@naver.com>
2025-04-08 01:35:30 +0900

    refactor: optimize Image component by using constant style objects

--- src/components/native/Image.tsx
+++ src/components/native/Image.tsx
@@ -1,36 +1,35 @@
-import React, { type ComponentProps, forwardRef } from 'react'
-import { type ImageStyle, Image as NativeImage, type StyleProp, type ViewStyle } from 'react-native'
-import { getClassName } from '../../core'
-import { maybeWarnAboutMultipleUnistyles } from '../../core/warn'
-import type { UnistylesValues } from '../../types'
-import { copyComponentProperties } from '../../utils'
-import { createUnistylesRef, keyInObject } from '../../web/utils'
+import React, { type ComponentProps, forwardRef } from "react";
+import { type ImageStyle, Image as NativeImage, type StyleProp, type ViewStyle } from "react-native";
+import { getClassName } from "../../core";
+import { maybeWarnAboutMultipleUnistyles } from "../../core/warn";
+import type { UnistylesValues } from "../../types";
+import { copyComponentProperties } from "../../utils";
+import { createUnistylesRef, keyInObject } from "../../web/utils";
+
+const defaultWidthStyle = { width: "" };
+const defaultHeightStyle = { height: "" };
 
 type Props = ComponentProps<typeof NativeImage> & {
-    style?: UnistylesValues
-    imageStyle?: UnistylesValues
-}
+    style?: UnistylesValues;
+    imageStyle?: UnistylesValues;
+};
 
 const UnistylesImage = forwardRef<unknown, Props>((props, forwardedRef) => {
-    const classNames = getClassName(props.style)
-    const ref = createUnistylesRef(classNames, forwardedRef)
-    const hasWidthStyle = typeof props.style === 'object' && keyInObject(props.style, 'width')
-    const hasHeightStyle = typeof props.style === 'object' && keyInObject(props.style, 'height')
+    const classNames = getClassName(props.style);
+    const ref = createUnistylesRef(classNames, forwardedRef);
+    const hasWidthStyle = typeof props.style === "object" && keyInObject(props.style, "width");
+    const hasHeightStyle = typeof props.style === "object" && keyInObject(props.style, "height");
 
-    maybeWarnAboutMultipleUnistyles(props.style as ViewStyle, 'Image')
+    const styleArray = [
+        classNames,
+        // Clear inline width and height extracted from source
+        hasWidthStyle && defaultWidthStyle,
+        hasHeightStyle && defaultHeightStyle,
+    ] as StyleProp<ImageStyle>;
 
-    return (
-        <NativeImage
-            {...props}
-            style={[
-                classNames,
-                // Clear inline width and height extracted from source
-                hasWidthStyle && { width: '' },
-                hasHeightStyle && { height: '' }
-            ] as StyleProp<ImageStyle>}
-            ref={ref}
-        />
-    )
-})
+    maybeWarnAboutMultipleUnistyles(props.style as ViewStyle, "Image");
 
-export const Image = copyComponentProperties(NativeImage, UnistylesImage)
+    return <NativeImage {...props} style={styleArray} ref={ref} />;
+});
+
+export const Image = copyComponentProperties(NativeImage, UnistylesImage);
