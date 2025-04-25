3f01a0f4c468635660d567c4b577e7f55244f8a4
Hubert Bieszczad <brentlokk@gmail.com>
2025-04-25 08:02:41 +0200

    fix: move animated web styles to inline

--- src/core/getClassname.ts
+++ src/core/getClassname.ts
@@ -1,15 +1,25 @@
 import type { UnistylesValues } from '../types'
 import { UnistylesWeb } from '../web'
+import { checkForAnimated } from '../web/utils'
 
 export const getClassName = (unistyle: UnistylesValues | undefined | Array<UnistylesValues>, forChild?: boolean) => {
     if (!unistyle) {
         return undefined
     }
 
+    const flattenedStyles = Array.isArray(unistyle) ? unistyle.flat(Number.POSITIVE_INFINITY) : [unistyle]
+    const animatedStyles = flattenedStyles.filter(checkForAnimated)
+    const regularStyles = flattenedStyles.filter(style => !checkForAnimated(style))
+
     const { hash, injectedClassName } = UnistylesWeb.shadowRegistry.addStyles(
-        Array.isArray(unistyle) ? unistyle.flat(Number.POSITIVE_INFINITY) : [unistyle],
+        regularStyles,
         forChild
     )
 
-    return hash ? { $$css: true, hash, injectedClassName } : undefined
+    return hash
+        ? [
+            { $$css: true, hash, injectedClassName },
+            animatedStyles,
+        ] as const
+        : undefined
 }
