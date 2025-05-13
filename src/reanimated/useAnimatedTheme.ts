9e042e8ef3b84acaae59eb0190b458098f00b35b
Hubert Bieszczad <brentlokk@gmail.com>
2025-05-13 10:05:17 +0200

    feat: web useAnimatedTheme

--- src/reanimated/useAnimatedTheme.ts
+++ src/reanimated/useAnimatedTheme.ts
@@ -1,20 +1,14 @@
 import { useEffect } from 'react'
 import { useSharedValue } from 'react-native-reanimated'
-import { StyleSheet, UnistyleDependency, UnistylesRuntime } from '../specs'
+import { UnistyleDependency, UnistylesRuntime } from '../specs'
 import type { UnistylesTheme } from '../types'
+import { services } from '../web/services'
 
 export const useAnimatedTheme = () => {
     const theme = useSharedValue<UnistylesTheme>(UnistylesRuntime.getTheme())
 
     useEffect(() => {
-        // @ts-ignore this is hidden from TS
-        const dispose = StyleSheet.addChangeListener(changedDependencies => {
-            if (changedDependencies.includes(UnistyleDependency.Theme)) {
-                theme.set(UnistylesRuntime.getTheme())
-            }
-        })
-
-        return () => dispose()
+        services.listener.addListeners([UnistyleDependency.Theme], () => theme.set(UnistylesRuntime.getTheme()))
     }, [])
 
     return theme
