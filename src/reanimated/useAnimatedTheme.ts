c0478b468e1cc6f1d000c7adfd6d819ab75ed169
Jacek Pudysz <jacekpudysz@gmail.com>
2025-05-13 12:03:31 +0200

    fix: import order

--- src/reanimated/useAnimatedTheme.ts
+++ src/reanimated/useAnimatedTheme.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { useSharedValue, type SharedValue } from 'react-native-reanimated'
+import { type SharedValue, useSharedValue } from 'react-native-reanimated'
 import { UnistyleDependency, UnistylesRuntime } from '../specs'
 import type { UnistylesTheme } from '../types'
 import { services } from '../web/services'
