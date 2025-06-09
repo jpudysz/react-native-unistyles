64a914b3b5320d8677b34639c4af6e0e8fd775ac
Jacek Pudysz <jacekpudysz@gmail.com>
2025-06-09 09:22:39 +0200

    chore: revert example to default

--- expo-example/app/(tabs)/index.tsx
+++ expo-example/app/(tabs)/index.tsx
@@ -1,65 +1,68 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { ScopedTheme, StyleSheet } from 'react-native-unistyles'
+import { Link } from 'expo-router'
+import { Pressable, View, Text } from 'react-native'
+import { StyleSheet, UnistylesRuntime } from 'react-native-unistyles'
 
 export default function HomeScreen() {
+    styles.useVariants({
+        variant: 'blue'
+    })
+
     return (
-        <View style={styles.container}>
-            <ScopedTheme name="premium">
-                    <Text style={styles.typography}>
-                        Nested premium theme
-                    </Text>
-                    <ScopedTheme name="dark">
-                        <Text style={styles.typography}>
-                            Nested dark theme
-                        </Text>
-                    </ScopedTheme>
-                </ScopedTheme>
-            <ScopedTheme invertedAdaptive>
-                <View style={styles.box}>
-                    <Text style={styles.typography}>
-                        This box has an accent color, so it should be pink in light mode and red in dark mode
-                    </Text>
-                </View>
+        <View style={styles.container(1)}>
+            <View style={styles.test}>
                 <Text style={styles.typography}>
-                    This text has a background color, so it should be dark for light mode and light for dark mode
+                    Hello world!
                 </Text>
-                <ScopedTheme name="premium">
-                    <Text style={styles.typography}>
-                        Nested premium theme
-                    </Text>
-                    <ScopedTheme name="light">
+                <Link href="/explore" asChild>
+                    <Pressable style={styles.button}>
                         <Text style={styles.typography}>
-                            Nested light theme
+                            Explore
                         </Text>
-                    </ScopedTheme>
-                </ScopedTheme>
-                <Text style={styles.typography}>
-                    Again scoped theme with invertedAdaptive
-                </Text>
-            </ScopedTheme>
+                    </Pressable>
+                </Link>
+                <Pressable onPress={() => UnistylesRuntime.getTheme()}>
+                    <Text style={styles.typography}>
+                        Press me
+                    </Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create(theme => ({
-    container: {
-        flex: 1,
+    container: (flex: number) => ({
+        flex,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 16,
-        gap: 12,
         backgroundColor: theme.colors.backgroundColor
-    },
+    }),
     typography: {
-        fontSize: 12,
-        textAlign: 'center',
-        color: theme.colors.backgroundColor
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: theme.colors.typography
     },
-    box: {
-        width: 100,
-        height: 100,
-        padding: 5,
-        backgroundColor: theme.colors.accent
+    test: {
+        width: '100%',
+        variants: {
+            variant: {
+                red: {
+                    backgroundColor: 'red'
+                },
+                blue: {
+                    backgroundColor: 'blue'
+                }
+            }
+        }
+    },
+    button: {
+        backgroundColor: theme.colors.aloes,
+        padding: 10,
+        borderRadius: 8,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%'
     }
 }))
