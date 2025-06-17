f965e5e824c65ef349c6ac4ecd61f67f1ef979ea
Jacek Pudysz <jacekpudysz@gmail.com>
2025-06-17 15:45:10 +0200

    feat: native implementation

--- expo-example/app/(tabs)/index.tsx
+++ expo-example/app/(tabs)/index.tsx
@@ -1,68 +1,59 @@
 import React from 'react'
-import { Link } from 'expo-router'
-import { Pressable, View, Text } from 'react-native'
-import { StyleSheet, UnistylesRuntime } from 'react-native-unistyles'
+import { View, Text } from 'react-native'
+import { ScopedTheme, StyleSheet, UnistylesRuntime } from 'react-native-unistyles'
 
-export default function HomeScreen() {
-    styles.useVariants({
-        variant: 'blue'
-    })
+const ScopedText = ({ prefix }: { prefix: string }) => {
+    const themeName = UnistylesRuntime.themeName
 
     return (
-        <View style={styles.container(1)}>
-            <View style={styles.test}>
-                <Text style={styles.typography}>
-                    Hello world!
-                </Text>
-                <Link href="/explore" asChild>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.typography}>
-                            Explore
-                        </Text>
-                    </Pressable>
-                </Link>
-                <Pressable onPress={() => UnistylesRuntime.getTheme()}>
-                    <Text style={styles.typography}>
-                        Press me
-                    </Text>
-                </Pressable>
-            </View>
+        <Text style={{ color: 'gray', fontSize: 20 }}>
+            {prefix}: I'm {themeName}
+        </Text>
+    )
+}
+
+const NextComponent = () => {
+    return (
+        <Text style={{ color: 'gray', fontSize: 20 }}>
+            NextComponent: I'm {UnistylesRuntime.themeName}
+        </Text>
+    )
+}
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <ScopedText prefix="Root" />
+            <ScopedTheme name="dark">
+                <ScopedText prefix="ScopedText" />
+            </ScopedTheme>
+            <ScopedTheme name="light">
+                <ScopedText prefix="ScopedText" />
+                <ScopedTheme name="premium">
+                    <ScopedText prefix="ScopedText" />
+                </ScopedTheme>
+            </ScopedTheme>
+            <ScopedTheme invertedAdaptive>
+                <ScopedText prefix="invertedAdaptive 1" />
+                <ScopedTheme invertedAdaptive>
+                    <ScopedText prefix="invertedAdaptive 2" />
+                </ScopedTheme>
+            </ScopedTheme>
+            <NextComponent />
         </View>
     )
 }
 
 const styles = StyleSheet.create(theme => ({
-    container: (flex: number) => ({
-        flex,
+    container: {
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: theme.colors.backgroundColor
-    }),
+    },
     typography: {
         fontSize: 20,
         fontWeight: 'bold',
         color: theme.colors.typography
-    },
-    test: {
-        width: '100%',
-        variants: {
-            variant: {
-                red: {
-                    backgroundColor: 'red'
-                },
-                blue: {
-                    backgroundColor: 'blue'
-                }
-            }
-        }
-    },
-    button: {
-        backgroundColor: theme.colors.aloes,
-        padding: 10,
-        borderRadius: 8,
-        height: 60,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%'
     }
 }))
