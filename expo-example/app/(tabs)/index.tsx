132a9bc6fcf278337e58eddade350a6016b4559b
Jacek Pudysz <jacekpudysz@gmail.com>
2025-06-18 11:14:41 +0200

    fix: prevent runtime updates in scoped theme when theme name changes

--- expo-example/app/(tabs)/index.tsx
+++ expo-example/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import { ScopedTheme, StyleSheet, useUnistyles, withUnistyles } from 'react-native-unistyles'
 
 const StyledText = withUnistyles(Text, (theme, rt) => ({
@@ -9,6 +9,8 @@ const StyledText = withUnistyles(Text, (theme, rt) => ({
 const ScopedText = ({ prefix, expected }: { prefix: string; expected: string }) => {
     const { rt } = useUnistyles()
 
+    rt.isPortrait
+
     return (
         <Text style={{ color: 'gray', fontSize: 20 }}>
             {prefix}: I'm {rt.themeName} ({expected})
@@ -17,11 +19,13 @@ const ScopedText = ({ prefix, expected }: { prefix: string; expected: string }) 
 }
 
 export default function HomeScreen() {
+    const [count, setCount] = React.useState(0)
+
     return (
         <View style={styles.container}>
             <ScopedText prefix="Root" expected="adaptive" />
-            <ScopedTheme name="dark">
-                <ScopedText prefix="ScopedText" expected="dark" />
+            <ScopedTheme name={count % 2 === 0 ? 'dark' : 'light'}>
+                <ScopedText prefix="ScopedText" expected={count % 2 === 0 ? 'dark' : 'light'} />
             </ScopedTheme>
             <ScopedTheme name="light">
                 <ScopedText prefix="ScopedText" expected="light" />
@@ -39,6 +43,7 @@ export default function HomeScreen() {
             <ScopedTheme invertedAdaptive>
                 <StyledText style={{ color: 'red' }} />
             </ScopedTheme>
+            <Button title="Increment" onPress={() => setCount(count + 1)} />
         </View>
     )
 }
