89bd71bc4a8d6ee9e4f2a14c0bd4d809265ad882
Aaron Holland <aaronholla@gmail.com>
2025-07-31 20:07:24 -0400

    fix: getWebProps forwardedRef missing

--- src/web-only/getWebProps.ts
+++ src/web-only/getWebProps.ts
@@ -3,9 +3,9 @@ import { getClassName } from '../core'
 import type { RNStyle, UnistylesValues } from '../types'
 import { createUnistylesRef } from '../web/utils/createUnistylesRef'
 
-export const getWebProps = <T>(style: StyleProp<RNStyle>) => {
+export const getWebProps = <T>(style: StyleProp<RNStyle>, forwardedRef?: React.ForwardedRef<T>) => {
     const styles = getClassName(style as UnistylesValues)
-    const ref = createUnistylesRef<T>(styles)
+    const ref = createUnistylesRef<T>(styles, forwardedRef)
     const [generatedStyles] = styles ?? []
 
     return {
