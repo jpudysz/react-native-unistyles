95884dce99622b7608513e02626d40587c1f90de
Jacek Pudysz <jacekpudysz@gmail.com>
2026-04-24 16:15:18 +0200

    fix: format

--- packages/unistyles/src/reanimated/useAnimatedTheme.native.ts
+++ packages/unistyles/src/reanimated/useAnimatedTheme.native.ts
@@ -18,7 +18,7 @@ export const useAnimatedTheme = () => {
     }
 
     useEffect(() => {
-        const dispose = StyleSheet.addChangeListener(changedDependencies => {
+        const dispose = StyleSheet.addChangeListener((changedDependencies) => {
             if (!changedDependencies.includes(UnistyleDependency.Theme)) {
                 return
             }
